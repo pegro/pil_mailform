00988c40f3974221a512f18143a476b40c23f8cd
Christian Lewin <christian.lewin@hdnet.de>
2016-05-18 15:02:29 +0200

    Made compatible with TYPO3 7.6

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,10 @@
 <?php
-if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
-t3lib_extMgm::addPItoST43($_EXTKEY,"pi1/class.tx_pilmailform_pi1.php","_pi1","list_type", 0);
-t3lib_extMgm::addTypoScript($_EXTKEY,"setup","plugin.".t3lib_extMgm::getCN($_EXTKEY)."_pi1 = USER_INT",43);
-?>
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+if (!defined("TYPO3_MODE")) {
+    die ("Access denied.");
+}
+ExtensionManagementUtility::addPItoST43($_EXTKEY, "pi1/class.tx_pilmailform_pi1.php", "_pi1", "list_type",
+    0);
+ExtensionManagementUtility::addTypoScript($_EXTKEY, "setup",
+    "plugin." . ExtensionManagementUtility::getCN($_EXTKEY) . "_pi1 = USER_INT", 43);
--- ext_tables.php
+++ ext_tables.php
@@ -1,14 +1,22 @@
 <?php
-if (!defined ("TYPO3_MODE")) 	die ("Access denied.");
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-t3lib_div::loadTCA("tt_content");
-t3lib_extMgm::allowTableOnStandardPages('pil_mailform');
+if (!defined("TYPO3_MODE")) {
+    die ("Access denied.");
+}
 
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages';
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
+ExtensionManagementUtility::allowTableOnStandardPages('pil_mailform');
 
-t3lib_extMgm::addPlugin(Array('LLL:EXT:pil_mailform/locallang.php:pil_mailform', $_EXTKEY.'_pi1'),'list_type');
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:pil_mailform/flexform_ds.xml');
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_pi1'] = 'layout,select_key,pages';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_pi1'] = 'pi_flexform';
 
-if (TYPO3_MODE=="BE") $TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_pilmailform_pi1_wizicon"] = t3lib_extMgm::extPath($_EXTKEY)."pi1/class.tx_pilmailform_pi1_wizicon.php";
-?>
+ExtensionManagementUtility::addPlugin(Array(
+    'LLL:EXT:pil_mailform/locallang.php:pil_mailform',
+    $_EXTKEY . '_pi1'
+), 'list_type');
+ExtensionManagementUtility::addPiFlexFormValue($_EXTKEY . '_pi1',
+    'FILE:EXT:pil_mailform/flexform_ds.xml');
+
+if (TYPO3_MODE == "BE") {
+    $TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["tx_pilmailform_pi1_wizicon"] = ExtensionManagementUtility::extPath($_EXTKEY) . "pi1/class.tx_pilmailform_pi1_wizicon.php";
+}

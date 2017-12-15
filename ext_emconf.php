483eb67ebf596ae937b3451092814c7df47bb4ce
Peter Große <pegro@fem-net.de>
2017-12-15 01:14:05 +0100

    version 4.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,11 +11,11 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = [
-    'title'              => 'TMailform (Unofficially patched by HDNET) for TYPO3 6.2-7.6 compatibility',
+    'title'              => 'TMailform (Unofficially patched by HDNET and pegro) for TYPO3 6.2-7.6 compatibility',
     'description'        => 'Template-based mailform that aims to give the developer the highest amount of design flexibility. Originally written by Tonni Aagesen (pil.dk).',
     'category'           => 'plugin',
     'shy'                => 0,
-    'version'            => '4.0.0',
+    'version'            => '4.0.1',
     'dependencies'       => '',
     'conflicts'          => '',
     'priority'           => '',
@@ -27,8 +27,8 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables'      => '',
     'clearcacheonload'   => 0,
     'lockType'           => '',
-    'author'             => 'Sebastian Busch, Christian Lewin',
-    'author_email'       => 'typo3-dev@billiton.de, christian.lewin@hdnet.de',
+    'author'             => 'Sebastian Busch, Christian Lewin, Peter Große',
+    'author_email'       => 'typo3-dev@billiton.de, christian.lewin@hdnet.de, pegro@friiks.de',
     'author_company'     => 'billiton internet services GmbH (http://www.billiton.de)',
     'CGLcompliance'      => '',
     'CGLcompliance_note' => '',
